37ffe7106a49fc7e2682151d10df318ee36119ca
Inderjit Shahi <inderjitshahi_mc20a7_13@dtu.ac.in>
2023-01-05 21:07:18 +0530

    newwsItem box height

--- components/NewsItem.js
+++ components/NewsItem.js
@@ -5,9 +5,9 @@ function NewsItem({ title, img, body,id }) {
     // console.log(body,img,title);
     return (
         <Link href={`/news/${id}`}>
-            <div className='flex border-2 h-32 justify-between items-center p-5 md:p-5 bg-white rounded-xl hover:bg-purple-200 active:scale-95 transition duration-100 ease-out'>
+            <div className='flex border-2  justify-between items-center p-5 md:p-5 bg-white rounded-xl hover:bg-purple-200 active:scale-95 transition duration-100 ease-out overflow-hidden'>
                 <div>
-                    <span className='font-semibold text-2xl'>{title}</span>
+                    <span className='font-semibold text-md md:text-2xl'>{title}</span>
                 </div>
                 <div>
                     <div className='relative h-16 w-16 rounded-lg overflow-hidden'>
